d5ce4bf5344ae59107d5e2c9f4ee62783b37b1f1
Abi2841 <abinashabinash77@gmail.com>
2023-03-25 01:33:40 +0530

    first commit

---
+++ static/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+urlpatterns = [
+    path('',views.home,name = 'home'),
+    path('contact/', views.contact, name='contact'),
+    path('works/', views.works, name='works'),
+    path('about/', views.about, name='about')
+]
---
+++ static/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request,'static/home.html')
+
+def works(request):
+    return render(request, 'static/ourworks.html')
+
+def about(request):
+    return render(request,'static/about.html')
+
+def contact(request):
+    return render(request, 'static/contact.html')
